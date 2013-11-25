de25db5a1f120ae702aafc9a76725e3aecf0c49c
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2013-11-24 19:09:34 -0500

    Utils

---
+++ md.wys.utils.js
@@ -0,0 +1,66 @@
+/* jshint laxcomma: true */
+
+(function() {
+  var utils = {};
+
+  /* unsurround selected text
+   *
+   * ___|matching outers|___
+   * TODO unsurround may need inner search |___matching outers___|
+   * dblclick text selection seems to select all between || in textareas
+   */
+
+  utils.unsurround = function(str, start, end) {
+    if (!is_surrounded.call(this, str, start, end)) {
+      return;
+    }
+
+    var strl = str.length
+      , ln = l.bind(this);
+
+    this.value = ln(0, start-strl)+ln(start, end)+ln(end+strl, this.value.length);
+  };
+
+
+  /* surround selected text
+   */
+
+  utils.surround = function(str, start, end) {
+    if (!str || start === end) {
+      return;
+    }
+
+    var ln = l.bind(this);
+
+    this.value = ln(0, start)+str+ln(start, end)+str+ln(end, this.value.length);
+  };
+
+
+  /* substring shortcut
+   */
+
+  function l(start, end) {
+    return this.value.substring(start, end);
+  }
+
+
+  /* checks for already existing surrounding
+   */
+
+   function is_surrounded(str, start, end) {
+    var strl = str.length
+      , sl = l.call(this, start-strl, start)
+      , er = l.call(this, end, end+strl);
+
+    return (sl === str && er === str);
+   }
+
+
+
+  //\/\
+  if ('object' === typeof module) {
+    module.exports = utils;
+  }
+
+}).call(this);
+
---
+++ test/md.wys.utils_test.js
@@ -0,0 +1,43 @@
+/* jshint laxcomma: true */
+
+var assert = require('assert')
+  , mdutils = require('../md.wys.utils');
+
+var editor = {
+  value: ''
+};
+
+
+describe('utils.surround', function() {
+  it('surrounds the selected word', function() {
+    editor.value = 'Hello World!';
+    mdutils.surround.call(editor, '__', 6, 11);
+
+    assert.equal(editor.value, 'Hello __World__!');
+  });
+
+  it('continues to surround the word', function() {
+    editor.value = 'Hello __World__!';
+    mdutils.surround.call(editor, '__', 8, 13);
+
+    assert.equal(editor.value, 'Hello ____World____!');
+  });
+});
+
+
+describe('utils.unsurround', function() {
+  it('unsurrounds the selected word', function() {
+    editor.value = 'Hello __World__!';
+    mdutils.unsurround.call(editor, '__', 8, 13);
+
+    assert.equal(editor.value, 'Hello World!');
+  });
+
+  it('only removes the matching surrouding string', function() {
+    editor.value = 'Hello World!';
+    mdutils.unsurround.call(editor, '__', 6, 11);
+
+    assert.equal(editor.value, 'Hello World!');
+  });
+});
+
